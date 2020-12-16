9e8895a5be9d1f33d6446c18454b5254dc0201f0
Bas van Baalen <bas.van.baalen@nn-group.com>
2020-12-16 15:42:36 +0100

    Removed unused code

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import Rating from '@material-ui/lab/Rating';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 
 import ReactMarkdown from "react-markdown";
@@ -12,54 +10,18 @@ export default function Details() {
 
   let { id } = useParams();
   const [document, setDocument] = useState({});
-  const [selectedTab] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setIsLoading(true);
-    // console.log(id);
     axios.get('/api/lookup?id=' + id)
       .then(response => {
         const doc = response.data.document;
         setDocument(doc);
-        setIsLoading(false);
       })
       .catch(error => {
         console.log(error);
-        setIsLoading(false);
       });
 
   }, [id]);
-
-
-  if (!isLoading && document) {
-    // View result
-    if (selectedTab === 0) {
-      resultStyle += " active";
-      detailsBody = (
-        <div className="card-body">
-          <h5 className="card-title">{document.original_title}</h5>
-          <img className="image" src={document.image_url} alt="Book cover"></img>
-          <p className="card-text">{document.authors?.join('; ')} - {document.original_publication_year}</p>
-          <p className="card-text">ISBN {document.isbn}</p>
-          <Rating name="half-rating-read" value={parseInt(document.average_rating)} precision={0.1} readOnly></Rating>
-          <p className="card-text">{document.ratings_count} Ratings</p>
-        </div>
-      );
-    }
-
-    // View raw data
-    else {
-      rawStyle += " active";
-      detailsBody = (
-        <div className="card-body text-left">
-          <pre><code>
-            {JSON.stringify(document, null, 2)}
-          </code></pre>
-        </div>
-      );
-    }
-  }
 
   return (
     <main className="main main--details container fluid">
